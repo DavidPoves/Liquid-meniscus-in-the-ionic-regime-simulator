b16cf6a8b9727a502deadaa2caba6dbc4d1c0e6b
David <54583380+DrDavxr@users.noreply.github.com>
2020-10-27 09:21:26 +0100

    Update distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
-	name='Liquid meniscus in the ionic regime ElectrospraySimulator',
-	version='2.0',
-	packages=['ElectrospraySimulator', 'ElectrospraySimulator.Tools', 'ElectrospraySimulator.Solvers',
-	          'ElectrospraySimulator.Main_scripts', 'ElectrospraySimulator.GUI_scripts',
-	          'ElectrospraySimulator.Geometry_scripts'],
-	url='https://github.com/DavidPoves/TFG-V2',
-	license='MIT',
-	author='DAVID POVES ROS',
-	author_email='daser1998@gmail.com',
-	description='Simulation of the liquid meniscus in the ionic regime of capillary electrospray thrusters.'
-)
+
+setuptools.setup(
+	name="Electrospray Simulator", # Replace with your own username
+	version="0.1",
+	author="David Poves Ros",
+	author_email="daser1998@gmail.com",
+	description="Simulator of electrospray thrusters",
+	long_description="Simulation of the liquid meniscus in the ionic regime of capillary electrospray thrusters.",
+	long_description_content_type="text/markdown",
+	url="https://github.com/DavidPoves/Liquid-meniscus-in-the-ionic-regime-simulator",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: POSIX :: Linux"
+	],
+	python_requires='>=3.8',
+	)
